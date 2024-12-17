b0d577d3dde92d414626332424a2e601d3068d88
lonecale <54177993+lonecale@users.noreply.github.com>
2024-12-17 17:17:28 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,19 +8,15 @@ mv */ /tmp/extd/
 # download feeds
 git clone https://github.com/sirpdboy/sirpdboy-package sirpdboy-package -b main --depth 1
 
-rm -rf sirpdboy-package/.git
-
+# 移动 luci-app-adguardhome 目录到当前目录
 mv sirpdboy-package/luci-app-adguardhome ./
 
+# 删除 sirpdboy-package/.git
+# rm -rf sirpdboy-package/.git
+rm -rf sirpdboy-package
 
-# 打印当前文件夹的内容
-ls -la
 
-
-# 打印 sirpdboy-package 目录内容，确认删除操作
-ls -la sirpdboy-package
-
-# 如果需要清理luci-app-adguardhome中的.git、LICENSE、README.md，可以取消注释下面这一行
+# 如果需要清理 luci-app-adguardhome 中的 .git、LICENSE、README.md，可以取消注释下面这一行
 # rm -rf luci-app-adguardhome/{.git,LICENSE,README.md}
 
 # 输出目录列表并保存到 packages.txt
